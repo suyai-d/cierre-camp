89da150a29c3462bfbd99a69b508d693a1d32904
Suyai <suyaiconci@gmail.com>
2026-05-29 13:45:03 -0300

    Update cierre-camp.py

--- cierre-camp.py
+++ cierre-camp.py
@@ -420,6 +420,7 @@ if activar_performance:
                     else:
                         st.caption("Sin datos de factor de carga.")
 
+
         st.markdown("---")
         st.header("2. Eficiencia de Operación y Tecnologías")
         st.markdown("### Comparativa de Tecnología de Guiado por Equipo")
