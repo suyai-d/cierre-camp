a2fd8974df6c1196e4186cadefc76c3c9d764141
Suyai <suyaiconci@gmail.com>
2026-07-01 11:23:17 -0300

    Update cierre-camp.py

--- cierre-camp.py
+++ cierre-camp.py
@@ -521,12 +521,12 @@ if activar_performance:
         st.markdown("---")
 
         # Seccion desplegable simple
-with st.expander("📝 Agregar Comentarios / Notas de la Campaña"):
-    comentarios = st.text_area(
-        label="Escribí acá tus observaciones para el informe:",
-        placeholder="Ej: El uso de tecnología en esta campaña fue óptimo...",
-        height=150
-    )        
+        with st.expander("📝 Agregar Comentarios / Notas de la Campaña"):
+            comentarios = st.text_area(
+                label="Escribí acá tus observaciones para el informe:",
+                placeholder="Ej: El uso de tecnología en esta campaña fue óptimo...",
+                height=150
+            )        
         st.header("2. Eficiencia de Operación y Tecnologías")
         st.markdown("### Comparativa de Tecnología de Guiado por Equipo")
 
